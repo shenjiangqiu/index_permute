c0156b5f61529c830f647cb6dbd0736bbc607896
Jiangqiu Shen <ivy22233qiu@live.com>
2025-07-20 17:49:42 -0400

    update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! - A tool to reorder an array by an index array inplace when the elements are not `Clone` or `Copy`.
+//! - A tool to reorder an array by an index array inplace when the elements are not [`Clone`] or [`Copy`].
 //! - lock-free parallel implementation.
 //! ## Example
 //! ```
@@ -18,12 +18,12 @@ use thiserror::Error;
 /// A struct to hold a permutation index.
 /// The index must be unique and in the range of `0..len`, where `len` is the length of the data to be permuted.
 /// This struct is used to ensure that the index is valid before performing any operations on the data.
-/// It can be created using `PermuteIndex::try_new`, which checks the validity of the index.
-/// If the index is invalid, it returns a `PermuteError::InvalidIndex` error.
-/// The index can be used to permute data using the `try_order_by_index_inplace` function.
-/// The index length must match the data length, otherwise it returns a `PermuteError::LengthMismatch` error.
-/// The `order_by_index_inplace` function is a convenience function that panics if the index is invalid or the lengths do not match.  
-/// # Example  
+/// It can be created using [`PermuteIndex::try_new`], which checks the validity of the index.
+/// If the index is invalid, it returns a [`PermuteError::InvalidIndex`] error.
+/// The index can be used to permute data using the [`try_order_by_index_inplace`] function.
+/// The index length must match the data length, otherwise it returns a [`PermuteError::LengthMismatch`] error.
+/// The [`order_by_index_inplace`] function is a convenience function that panics if the index is invalid or the lengths do not match.
+/// # Example
 /// ```
 /// use index_permute::PermuteIndex;
 /// let index = PermuteIndex::try_new(&[2, 0, 1
@@ -36,7 +36,7 @@ pub struct PermuteIndex<T> {
     data: T,
 }
 
-/// An error type for `PermuteIndex` and `try_order_by_index_inplace`.
+/// An error type for [`PermuteIndex`] and [`try_order_by_index_inplace`].
 #[derive(Debug, Error)]
 pub enum PermuteError {
     /// An error indicating that the index is invalid.
@@ -63,12 +63,12 @@ where
         }
         true
     }
-    /// Creates a new `PermuteIndex` if the index is valid.
-    /// Returns `PermuteError::InvalidIndex` if the index is not valid.
+    /// Creates a new [`PermuteIndex`] if the index is valid.
+    /// Returns [`PermuteError::InvalidIndex`] if the index is not valid.
     /// The index must be unique and in the range of `0..len`, where `len` is the length of the data to be permuted.
-    /// The index can be used to permute data using the `try_order_by_index_inplace` function.
-    /// The index length must match the data length, otherwise it returns a `PermuteError::LengthMismatch` error.
-    /// The `order_by_index_inplace` function is a convenience function that panics if the index is invalid or the lengths do not match.
+    /// The index can be used to permute data using the [`try_order_by_index_inplace`] function.
+    /// The index length must match the data length, otherwise it returns [`PermuteError::LengthMismatch`] error.
+    /// The [`order_by_index_inplace`] function is a convenience function that panics if the index is invalid or the lengths do not match.
     /// # Example
     /// ```
     /// use index_permute::PermuteIndex;
@@ -87,13 +87,13 @@ where
 }
 
 /// Reorders the data in place according to the given index.
-/// First create a `PermuteIndex`, then, it reorders the data in place
+/// First create a [`PermuteIndex`], then, it reorders the data in place
 /// # Example
 /// ```
 /// use index_permute::PermuteIndex;
 /// let index = PermuteIndex::try_new(&[2, 0, 1]).unwrap();
 /// let mut data = vec![10, 20, 30];
-/// index_permute::order_by_index_inplace(&mut data, index);
+/// index_permute::try_order_by_index_inplace(&mut data, index).unwrap();
 /// assert_eq!(data, vec![30, 10, 20]);
 /// ```
 pub fn try_order_by_index_inplace<T, I>(
@@ -135,7 +135,7 @@ where
 }
 
 /// A convenience function that panics if the index is invalid or the lengths do not match.
-/// It is recommended to use `try_order_by_index_inplace` for error handling.
+/// It is recommended to use [`try_order_by_index_inplace`] for error handling.
 /// # Example
 /// ```
 /// use index_permute::PermuteIndex;
